ef577952c77ad78089018b4b960a38c1db475bef
yahyasaadi <yahyasaadi9219@gmail.com>
2022-02-12 10:23:26 +0300

    added a register route

--- blog_app/routes.py
+++ blog_app/routes.py
@@ -1,8 +1,16 @@
-from flask import render_template
+from flask import redirect, render_template, url_for
 from blog_app import app
+from blog_app.forms import RegistrationForm, LoginForm
 
 # routes
 @app.route('/')
 def home():
     title="Home"
     return render_template('home.html', title=title)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form, title='Register Form')
