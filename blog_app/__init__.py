1630b48250f64b7e06f316af05a03dd809955766
yahyasaadi <yahyasaadi9219@gmail.com>
2022-02-12 12:19:24 +0300

    added a db url

--- blog_app/__init__.py
+++ blog_app/__init__.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ghnkjilo765fhy87hbgtr45'
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
-
+db = SQLAlchemy(app)
 
 from blog_app import routes
