a429917aa3937ea02d1614e1296c8de04d0dbfaa
yahyasaadi <yahyasaadi9219@gmail.com>
2022-02-14 11:03:41 +0300

    added a user_loader and made some corrections

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,13 +1,22 @@
 from datetime import datetime
-from blog_app import db
+from blog_app import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
-    comments = db.relationship('Comment', backref='article', lazy=True)
+    comments = db.relationship('Comment', backref='user_comment', lazy=True)
 
 
     def __repr__(self):
@@ -20,7 +29,7 @@ class Post(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    comments = db.relationship('Comment', backref='article', lazy=True)
+    comments = db.relationship('Comment', backref='post_comment', lazy=True)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
@@ -30,7 +39,7 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    post_id = db.Column(db.Text, db.ForeignKey('post.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
