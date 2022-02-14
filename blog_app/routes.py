fe01b31147a8ae6acd687d69754310c2b36165dc
yahyasaadi <yahyasaadi9219@gmail.com>
2022-02-14 18:04:51 +0300

    added an update and delete routes

--- blog_app/routes.py
+++ blog_app/routes.py
@@ -1,4 +1,4 @@
-from flask import redirect, render_template, url_for, flash
+from flask import redirect, render_template, request, url_for, flash, abort
 from blog_app import app, db, bcrypt, login_manager
 from blog_app.forms import RegistrationForm, LoginForm, PostForm
 from blog_app.models import User, Post, Comment
@@ -69,3 +69,34 @@ def new_post():
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', post=post)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('new_post.html', form=form, legend='Update Post')
+
+
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Post deleted successfully', 'info')
+    return redirect(url_for('home'))
