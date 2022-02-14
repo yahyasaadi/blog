8883c5c86cff20bc6fa70e54930378abec1032e6
yahyasaadi <yahyasaadi9219@gmail.com>
2022-02-14 11:02:32 +0300

    Added a db set annd initialized it

--- blog_app/__init__.py
+++ blog_app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ghnkjilo765fhy87hbgtr45'
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://yahya:Noor_main_db@localhost/blog'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from blog_app import routes
