702a328d72ef907d07b2aaa04687b49a47d1c55d
yahyasaadi <yahyasaadi9219@gmail.com>
2022-02-15 17:10:33 +0300

    imported config file to hide db and secret key

--- blog_app/__init__.py
+++ blog_app/__init__.py
@@ -2,11 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from blog_app.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ghnkjilo765fhy87hbgtr45'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://yahya:Noor_main_db@localhost/blog'
-
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
